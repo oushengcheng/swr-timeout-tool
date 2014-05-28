1fe7e225657ec7a70b01a95bc973b9248438c916
Andrew Spratley <aps831@yahoo.co.uk>
2014-05-28 12:48:54 +0100

    Test for logged in before testing role etc in AuthoriZationChecker

--- src/main/java/com/aps/wicc/ejb/security/AuthorizationChecker.java
+++ src/main/java/com/aps/wicc/ejb/security/AuthorizationChecker.java
@@ -23,18 +23,47 @@ public class AuthorizationChecker
     private RelationshipManager relationshipManager;
     
     public boolean hasApplicationRole(final String roleName) {
-        final Role role = BasicModel.getRole(this.identityManager, roleName);
+    	
+    	if (!identity.isLoggedIn()) {
+    		
+    		return false;
+    		
+    	}
+    		        
+    	final Role role = BasicModel.getRole(this.identityManager, roleName);
+        
         return BasicModel.hasRole(this.relationshipManager, this.identity.getAccount(), role);
+        
     }
     
     public boolean isMember(final String groupName) {
-        final Group group = BasicModel.getGroup(this.identityManager, groupName);
-        return BasicModel.isMember(this.relationshipManager, this.identity.getAccount(), group);
+    	
+    	if (!identity.isLoggedIn()) {
+    		
+    		return false;
+    		
+    	}
+    		        
+    	final Group group = BasicModel.getGroup(this.identityManager, groupName);
+        
+    	return BasicModel.isMember(this.relationshipManager, this.identity.getAccount(), group);
+    	
     }
     
     public boolean hasGroupRole(final String roleName, final String groupName) {
-        final Group group = BasicModel.getGroup(this.identityManager, groupName);
-        final Role role = BasicModel.getRole(this.identityManager, roleName);
-        return BasicModel.hasGroupRole(this.relationshipManager, this.identity.getAccount(), role, group);
+    	
+    	if (!identity.isLoggedIn()) {
+    		
+    		return false;
+    		
+    	}
+    		        
+    	final Group group = BasicModel.getGroup(this.identityManager, groupName);
+        
+    	final Role role = BasicModel.getRole(this.identityManager, roleName);
+        
+    	return BasicModel.hasGroupRole(this.relationshipManager, this.identity.getAccount(), role, group);
+    	
     }
+    
 }
