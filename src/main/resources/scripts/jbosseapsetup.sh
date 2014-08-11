4e246a1c2c07e3a011a64fd27a94bdf0c6695775
Andrew Spratley <aps831@yahoo.co.uk>
2014-08-12 00:42:57 +0100

    Removed storing of mysql connector in scripts folder

--- src/main/resources/scripts/jbosseapsetup.sh
+++ src/main/resources/scripts/jbosseapsetup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 export JBOSS_HOME='/opt/jboss-eap-6.1'
 
-mysqlconnector=mysql-connector-java-5.1.25-bin.jar
 jbosscliconnect='/opt/jboss-eap-6.1/bin/jboss-cli.sh -c'
 
+MYSQL_CONNECTOR_GROUPID=mysql
 MYSQL_CONNECTOR_VERSION=5.1.25
 MYSQL_CONNECTOR_NAME=mysql-connector-java
-MYSQL_CONNECTOR_FULL_NAME=$MYSQL_CONNECTOR_NAME-$MYSQL_CONNECTOR_VERSION-bin.jar
+MYSQL_CONNECTOR_PACKAGING=jar
+MYSQL_CONNECTOR_FULL_NAME=$MYSQL_CONNECTOR_NAME-$MYSQL_CONNECTOR_VERSION.$MYSQL_CONNECTOR_PACKAGING
+MYSQL_CONNECTOR_FULL_NAME_AND_PATH=$JBOSS_HOME/$MYSQL_CONNECTOR_FULL_NAME
 DEPENDENCY_PLUGIN_VERSION=2.8
-MAVEN_DEPENDENCY_PLUGIN=org.apache.maven.plugins:maven-dependency-plugin:2.8
+MAVEN_DEPENDENCY_PLUGIN_AND_GOAL=org.apache.maven.plugins:maven-dependency-plugin:2.8:copy
 
 #
 # Read in username & password
@@ -18,11 +20,11 @@ MAVEN_DEPENDENCY_PLUGIN=org.apache.maven.plugins:maven-dependency-plugin:2.8
 # mysql -uroot -prootpw -e "GRANT ALL PRIVILEGES ON dbname.* TO username@hostname IDENTIFIED BY 'userpassword'"
 # FLush priveliges
 
-# Place mysql connector to JBOSS_HOME
-mvn $MAVEN_DEPENDENCY_PLUGIN:copy -Dartifact=mysql:mysql-connector-java:5.1.25:jar -DoutputDirectory=$JBOSS_HOME
+# Place mysql connector in JBOSS_HOME
+mvn $MAVEN_DEPENDENCY_PLUGIN_AND_GOAL -Dartifact=$MYSQL_CONNECTOR_GROUPID:$MYSQL_CONNECTOR_NAME:$MYSQL_CONNECTOR_VERSION:$MYSQL_CONNECTOR_PACKAGING -DoutputDirectory=$JBOSS_HOME
 
 echo "Adding MySQL module"
-$jbosscliconnect --command="module add --name=com.mysql --resources=$JBOSS_HOME/$mysqlconnector --dependencies=javax.api,javax.transaction.api"
+$jbosscliconnect --command="module add --name=com.mysql --resources=$MYSQL_CONNECTOR_FULL_NAME_AND_PATH --dependencies=javax.api,javax.transaction.api"
 
 echo "Adding MySQL jdbc driver"
 $jbosscliconnect --command='/subsystem=datasources/jdbc-driver=mysql:add(driver-module-name=com.mysql, driver-name=mysql, driver-class-name=com.mysql.jdbc.Driver)'
@@ -37,4 +39,5 @@ echo "Testing datasource"
 $jbosscliconnect --command='/subsystem=datasources/data-source=wicc:test-connection-in-pool'
 
 # Remove temporary copy of mysqlconnector
-rm "$JBOSS_HOME/$mysqlconnector"
+rm $MYSQL_CONNECTOR_FULL_NAME_AND_PATH
+
