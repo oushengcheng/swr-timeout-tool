c4d19107b25cd584fc5be7d64400ba1a117bc371
Andrew Spratley <aps831@yahoo.co.uk>
2014-10-05 08:08:23 +0100

    Removed scaling from Openshift set up scripts

--- src/main/resources/scripts/create_openshift_app_prod.sh
+++ src/main/resources/scripts/create_openshift_app_prod.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-rhc create-app -s timeouttool jbosseap-6 mysql-5.5 -g medium -n swrwicc --region aws-eu-west-1
+rhc create-app timeouttool jbosseap-6 mysql-5.5 -g medium -n swrwicc --region aws-eu-west-1
--- src/main/resources/scripts/create_openshift_app_staging.sh
+++ src/main/resources/scripts/create_openshift_app_staging.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-rhc create-app -s timeouttool jbosseap-6 mysql-5.5 -g small -n andrewspratley --region aws-eu-west-1
+rhc create-app timeouttool jbosseap-6 mysql-5.5 -g medium -n swrtest --region aws-eu-west-1
