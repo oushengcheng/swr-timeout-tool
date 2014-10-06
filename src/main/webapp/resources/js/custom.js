4ae666ccc86ce2899c99ff2fb99ec424911d889e
Andrew Spratley <aps831@yahoo.co.uk>
2014-10-06 20:27:07 +0100

    Removed hardcoding of screen size for marquee

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -7,8 +7,19 @@
     aps.marquee = function marquee(speed) {
 
         var total_height = $("#scrollwindow")[0].scrollHeight;
-        var visible_height = $("#scrollwindow").height();
-
+        //var visible_height = $("#scrollwindow").height();
+        
+        var scroll_window_top = $("#scrollwindow").position().top;
+        var footer_top = $("#footer").position().top;
+        
+        var space = sum_of_bottom_spacing("#wrap");
+        space = space + sum_of_bottom_spacing("#wrap > .container");
+        space = space + sum_of_bottom_spacing(".planviewscroll");
+        
+        var visible_height = footer_top - scroll_window_top - space;
+        
+        $("#scrollwindow").css({'height' : visible_height});
+        
         var count = 0;
 
         var duration = (visible_height + total_height) / speed;
@@ -51,4 +62,10 @@
 
     };
 
+    function sum_of_bottom_spacing(selector) {
+        var space = $(selector).outerHeight(true) - $(selector).height();
+        space = space / 2             
+        return space;
+    }
+    
 }(window.aps = window.aps || {}, jQuery));
